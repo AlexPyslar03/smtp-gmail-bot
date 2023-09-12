ce1a76d05fa0c09db3132361c378eaadf2adb416
Александр Пысларь <62560925+AlexPyslar03@users.noreply.github.com>
2023-09-12 23:58:53 +0300

    tgbth

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-host = "smtp.yandex.com" # Хост сервиса откуда идёт отправка писем
+host = "smtp.gmail.com" # Хост сервиса откуда идёт отправка писем
 port = 587 # Порт сервиса
 
-from_email = "alexpyslar03@yandex.ru" # Почта откуда отправляется письмо
-password = "ijesowcspbwecbmj" # Ключь пароль почты
+from_email = "" # Почта откуда отправляется письмо
+password = "" # Ключь пароль почты
 
 msg_subject = "Cooperation" # Заголовок письма
 msg_text =  """Hello, I'm a graphic designer, I create YouTube video thumbnails for channels. I have been working in this field for a long time, so I have an understanding of how to improve the quality and clickability of your thumbnails (including CTR) to set you apart from your competitors.
@@ -15,3 +15,5 @@ Best wishes!"""
 
 xlsx_file_name = "emails_db.xlsx" # Название Excel файла
 xlsx_sheet_name = "emails_db" # Название листа
+
+time_sleep = 3.0 # Время ожидания
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from config import * # Файл конфигурации пользовательских данных
 import pandas as pd
+import time
 
 # Создание server'а обработки
 server = smtplib.SMTP(host, port)  # Создание объекта загрузки
@@ -18,17 +19,16 @@ def send_email(to_email): # Функция отправки сообщения
         msg['From'] = from_email
         msg['To'] = to_email
         msg['Subject'] = msg_subject # Добавление заголовка письма
-        #msg.attach(MIMEText(msg_text, 'plain')) # Добавление текста письма
-        msg.attach(MIMEImage(file("thumbnails1.jpg").read()))
+        msg.attach(MIMEText(msg_text, 'plain')) # Добавление текста письма
         server.sendmail(from_email, to_email, msg.as_string()) # Отправка письма
         return "Письмо отправленно на почту " + to_email # Возвращение сообщения об успехе
     except Exception as _ex: # Обработка исключений
         return "!Ошибка при отправки на почту " + to_email + " проверьте логин или пароль"
 
 xlsx = pd.read_excel(xlsx_file_name, sheet_name=xlsx_sheet_name)
-print(send_email('alexpyslar03@yandex.ru'))
-#for i in range(0, xlsx.values.size):
-    #if (str(xlsx.values.item(i)) != "nan"):
-        #print(str(i + 2) + "-ый элемент в таблицы: " + send_email(to_email=xlsx.values.item(i)))
+for i in range(0, xlsx.values.size):
+    if (str(xlsx.values.item(i)) != "nan"):
+        print(str(i + 2) + "-ый элемент в таблицы: " + send_email(to_email=xlsx.values.item(i)))
+        time.sleep(time_sleep)
 
 server.quit()
